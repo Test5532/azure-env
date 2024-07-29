f554b694caa3d3f29e222ac5af392322b3dec279
test5532 <test5532>
2024-07-29 23:29:48 +0530

    default value added

--- provider.tf
+++ provider.tf
@@ -3,9 +3,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.113.0"
-      features{}
+
       skip_provider_registration = true
       
     }
